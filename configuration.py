e3df7e3e0a26697a1763f17ec4f739216829e312
Гунька Ксения Андреевна <gunka@intellex.ru>
2023-01-11 15:22:00 +0300

    My first commit

---
+++ configuration.py
@@ -0,0 +1,6 @@
+URL_SERVICE = "https://2a1d9dad-f3be-4a7e-9241-0e4aaecbd993.serverhub.praktikum-services.ru"
+DOC_PATH = "/docs/"
+LOG_MAIN_PATH = "/api/logs/main/"
+USERS_TABLE_PATH = "/api/db/resources/user_model.csv"
+CREATE_USER_PATH = "/api/v1/users/"
+PRODUCTS_KITS_PATH = "/api/v1/products/kits/"
